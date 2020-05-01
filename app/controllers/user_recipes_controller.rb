85ef06245c8d8c9aa49a1ac45cbb3c11282e8178
komald4 <komald.4@gmail.com>
2020-05-01 16:23:56 -0400

    Routes

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,3 +1,23 @@
 class UserRecipesController < ApplicationController
+    def create
+        userrecipes = UserRecipe.create(userrecipe_params)
+        render json: userrecipe, except: [:updated_at, :created_at]
+    end
     
+    def index
+        userrecipes = UserRecipe.all
+        render :json => userrecipe, :include => ['student', 'doctor']
+    end
+
+    def destroy
+        @userrecipe = UserRecipe.find(params[:id])
+        @userrecipe.destroy
+        render json: @userrecipe, except: [:updated_at, :created_at]
+    end
+
+    private
+    def userrecipe_params
+        params.require(:userrecipe).permit(:id, :student_id, :doctor_id, :start_date, :length, :accepted)
+    end
+end
 end
